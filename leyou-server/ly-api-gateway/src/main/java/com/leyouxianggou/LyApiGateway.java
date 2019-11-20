eb8c27265777e3037119cfaac74f6d7516faac3e
Liu Qingyuan <wsyslf1996@qq.com>
2019-11-20 22:57:29 +0800

    乐优享购项目 建项

---
+++ leyou-server/ly-api-gateway/src/main/java/com/leyouxianggou/LyApiGateway.java
@@ -0,0 +1,11 @@
+package com.leyouxianggou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class LyApiGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyApiGateway.class,args);
+    }
+}
