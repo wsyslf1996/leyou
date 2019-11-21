03a710347e35ce045d735c4f1c783f9733447c4a
Liu Qingyuan <wsyslf1996@qq.com>
2019-11-21 22:09:11 +0800

    项目大体框架搭建

---
+++ leyou-server/ly-item/ly-item-service/src/main/java/com/leyouxianggou/LyItemApplication.java
@@ -0,0 +1,14 @@
+package com.leyouxianggou;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class LyItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemApplication.class,args);
+    }
+}
